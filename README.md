7005b5b869681480f822dc682f00b469af5c0827
Igor Nikolaychuk <jmpeax@icloud.com>
2018-03-13 01:42:51 +0200

    new promise api

--- README.md
+++ README.md
@@ -5,61 +5,54 @@ Based on [argon2][1].
 
 ###Module usage examples with [supervisor][2] repl interface
 
-#### Create promise:
+#### Constructing promise:
 Request:
 ```javascript
-services.promise.createPromise({
- "promise_alg": "trustkey_argon2d",
- "server_id": "D000000000000000",
- "trustkey_ts": 1520364270,
- "time_cost": 20,
- "memory_cost": 3,
- "parallelism": 1,
- "n_bytes": 64,
- "b64_buffers": true
-}, (res) => console.log(res))
+‌‌‌‌services.promise.resolve({
+      "_f": "trustkey_argon2d_constructor",
+      "server_id": "D000000000000000",
+      "trustkey_ts": 1520364270,
+      "time_cost": 20,
+      "memory_cost": 3,
+      "parallelism": 1,
+      "n_bytes": 64,
+      "b64_buffers": true
+  }, (res) => console.log(res))
 ```
 
 `Argon2 parameters are shown as an example. The only possible attacker is the server therefore secure parameters should be chosen by users`
 
 Response:
 ```json
-{
-  "success": true,
-  "result": {
-    "promise_alg": "trustkey_argon2d",
-    "server_id": "D000000000000000",
-    "trustkey_ts": 1520364270,
-    "time_cost": 20,
-    "memory_cost": 3,
-    "parallelism": 1,
-    "b64_buffers": true,
-    "seed": "TRqE8+TrmWEzsFOzcJ+c8c4bYK2mlpucYDYmoG1D80QIye+y+6OhcxsEEhuSLclbCjbZzq1X53\/xFB+Za5XPvg=="
-  }
-}
+{ "success": true,
+  "result": 
+   { "_f": "trustkey_argon2d",
+     "server_id": "D000000000000000",
+     "trustkey_ts": 1520364270,
+     "time_cost": 20,
+     "memory_cost": 3,
+     "parallelism": 1,
+     "b64_buffers": true,
+     "seed": "ScmnLtFrnCxaLW5FRipvJEDZ6VZ8k9HFxR6l0esIpWx3sRV6uNJI+FqFfmcSHDZokBvuXBrdgopoX+9GlIRaEA==" } }
 ```
 
 #### Resolve promise:
 Request:
 ```javascript
 //Based on previous response
-services.promise.resolvePromise({
-  "promise_alg": "trustkey_argon2d",
-  "server_id": "D000000000000000",
-  "trustkey_ts": 1520364270,
-  "time_cost": 20,
-  "memory_cost": 3,
-  "parallelism": 1,
-  "b64_buffers": true,
-  "seed": "TRqE8+TrmWEzsFOzcJ+c8c4bYK2mlpucYDYmoG1D80QIye+y+6OhcxsEEhuSLclbCjbZzq1X53\/xFB+Za5XPvg=="
-}, (res) => console.log(res))
+‌‌services.promise.resolve({ "_f": "trustkey_argon2d",
+    "server_id": "D000000000000000",
+    "trustkey_ts": 1520364270,
+    "time_cost": 20,
+    "memory_cost": 3,
+    "parallelism": 1,
+    "b64_buffers": true,
+    "seed": "ScmnLtFrnCxaLW5FRipvJEDZ6VZ8k9HFxR6l0esIpWx3sRV6uNJI+FqFfmcSHDZokBvuXBrdgopoX+9GlIRaEA==" }, (res) => console.log(res))
 ```
 Response:
 ```json
-{
-  "success": true,
-  "result": "CdAiOkVk39FdCM+74DlgEOgIp21RgR5jyAw6TpL36SMrVat7PhnpPRXskI9eKWu1j3us7q1iY+JQy5hZQA9DoA=="
-}
+{ "success": true,
+  "result": "DQMB53Dk2pw0lfJN1oyTxWbKLpaLhFQ6bSS5PxS8vwtULVGzfWgAtlRt/PPeGJSGFVabfBroBhfJgGiGvx7WDg==" }
 ```
 
 Error response example(error codes listed in `errorCodes.js`):
--- module.js
+++ module.js
@@ -55,136 +55,127 @@ const resolvePromiseRequestSchema = {
 
 const errorCodes = require('./errorCodes');
 
-class Argon2dTrustkeyPromiseService {
-    constructor(imports) {
-        this.core = imports.core;
-        this.rng = imports.rng;
-        this.name = "trustkey_argon2d";
-    }
+module.exports = function setup(options, imports, register) {
+    const trustkey_argon2d_constructor =  {
+        name: "trustkey_argon2d_constructor",
+        resolve: (request, resolve, reject) => {
+            //Recheck promise params and generate seed
 
-    createPromise(request, callback) {
-        //Recheck promise params and generate seed
+            const vRes = v.validate(request, createPromiseRequestSchema);
 
-        const vRes = v.validate(request, createPromiseRequestSchema);
+            let response = { };
 
-        let response = { };
+            const respondWithError = (ec) => {
+                response.error_code = ec;
+                response.error = errorCodes[ec];
 
-        const respondWithError = (ec) => {
-            response.success = false;
-            response.error_code = ec;
-            response.error = errorCodes[ec];
-
-            callback(response);
-        };
-
-        if(vRes.errors.length) {
-            response.validation_errors = vRes.errors;
-            return respondWithError(1);
-        }
-
-        let supervisor = this.core.getSupervisorByServerId(request.server_id);
-
-        if(!supervisor.success || !supervisor.result.connected)
-            return respondWithError(2);
-
-        supervisor = supervisor.result;
-
-        if((request.trustkey_ts % supervisor.roundTime) !== 0)
-            return respondWithError(3);
-
-        request.seed = this.rng.generate(request.n_bytes);
-
-        delete request.n_bytes;
-
-        response.success = true;
-        response.result = request;
-
-        callback(response);
-    }
-
-    resolvePromise(request, callback) {
-        const vRes = v.validate(request, resolvePromiseRequestSchema);
-
-        let response = {};
-
-        const respondWithError = (ec) => {
-            response.success = false;
-            response.error_code = ec;
-            response.error = errorCodes[ec];
-
-            callback(response);
-        };
-
-        if(vRes.errors.length) {
-            response.validation_errors = vRes.errors;
-            return respondWithError(1);
-        }
-
-        let seedBytes = request.seed;
-
-        if(typeof (seedBytes) === 'string')
-            seedBytes = Buffer.from(seedBytes, 'base64');
-
-        if(!Buffer.isBuffer(seedBytes))
-            return respondWithError(7);
-
-        let supervisor = this.core.getSupervisorByServerId(request.server_id);
-
-        if(!supervisor.success)
-            return respondWithError(2);
-
-        supervisor = supervisor.result;
-
-        supervisor.trustkeysCollection.findOne({ts: request.trustkey_ts}, (err, res) => {
-            if(err) {
-                response.db_error = err;
-                return respondWithError(4);
-            }
-
-            if(!res) {
-                return respondWithError(6);
-            }
-
-            if(!res.is_trusted) {
-                response.trustkey = res;
-                return respondWithError(5);
-            }
-
-            let options = {
-                type: argon2.argon2d,
-                raw: true,
-                salt: new Buffer(res.trustkey, "hex")
+                reject(response);
             };
 
-            if(request.time_cost)
-                options.timeCost = request.time_cost;
+            if(vRes.errors.length) {
+                response.validation_errors = vRes.errors;
+                return respondWithError(1);
+            }
 
-            if(request.memory_cost)
-                options.memoryCost = request.memory_cost;
+            let supervisor = imports.core.getSupervisorByServerId(request.server_id);
 
-            if(request.parallelism)
-                options.parallelism = request.parallelism;
+            if(!supervisor.success || !supervisor.result.connected)
+                return respondWithError(2);
 
-            options.hashLength = seedBytes.length;
+            supervisor = supervisor.result;
 
-            argon2.hash(res.inputs.buffer, options).then(function (res) {
-                const bytes = xorInplace(seedBytes, res);
+            if((request.trustkey_ts % supervisor.roundTime) !== 0)
+                return respondWithError(3);
 
-                callback({
-                    success: true,
-                    result: bytes
+            request.seed = imports.rng.generate(request.n_bytes);
+            request._f = "trustkey_argon2d";
+
+            delete request.n_bytes;
+
+            resolve(request);
+        }
+    };
+
+    const trustkey_argon2d = {
+        name: "trustkey_argon2d",
+        resolve: (request, resolve, reject) => {
+            const vRes = v.validate(request, resolvePromiseRequestSchema);
+
+            let response = {};
+
+            const respondWithError = (ec) => {
+                response.error_code = ec;
+                response.error = errorCodes[ec];
+
+                reject(response);
+            };
+
+            if(vRes.errors.length) {
+                response.validation_errors = vRes.errors;
+                return respondWithError(1);
+            }
+
+            let seedBytes = request.seed;
+
+            if(typeof (seedBytes) === 'string')
+                seedBytes = Buffer.from(seedBytes, 'base64');
+
+            if(!Buffer.isBuffer(seedBytes))
+                return respondWithError(7);
+
+            let supervisor = imports.core.getSupervisorByServerId(request.server_id);
+
+            if(!supervisor.success)
+                return respondWithError(2);
+
+            supervisor = supervisor.result;
+
+            supervisor.trustkeysCollection.findOne({ts: request.trustkey_ts}, (err, res) => {
+                if(err) {
+                    response.db_error = err;
+                    return respondWithError(4);
+                }
+
+                if(!res) {
+                    return respondWithError(6);
+                }
+
+                if(!res.is_trusted) {
+                    response.trustkey = res;
+                    return respondWithError(5);
+                }
+
+                let options = {
+                    type: argon2.argon2d,
+                    raw: true,
+                    salt: new Buffer(res.trustkey, "hex")
+                };
+
+                if(request.time_cost)
+                    options.timeCost = request.time_cost;
+
+                if(request.memory_cost)
+                    options.memoryCost = request.memory_cost;
+
+                if(request.parallelism)
+                    options.parallelism = request.parallelism;
+
+                options.hashLength = seedBytes.length;
+
+                argon2.hash(res.inputs.buffer, options).then(function (res) {
+                    const bytes = xorInplace(seedBytes, res);
+
+                    resolve(bytes);
                 });
             });
-        });
-    }
+        }
+    };
 
-}
+    imports.promise.postAlgorithm(trustkey_argon2d_constructor);
+    imports.promise.postAlgorithm(trustkey_argon2d);
 
-module.exports = function setup(options, imports, register) {
-    const alg = new Argon2dTrustkeyPromiseService(imports);
-
-    imports.promise.postAlgorithm(alg);
     register(null, {
-        trustkey_argon2d: alg
+        trustkey_argon2d_constructor: trustkey_argon2d_constructor,
+        trustkey_argon2d: trustkey_argon2d
     });
 };
